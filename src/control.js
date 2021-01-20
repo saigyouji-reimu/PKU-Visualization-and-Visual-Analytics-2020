47e178954af58c35552231ca2d4e23639781b429
xq114 <1140735506@qq.com>
2021-01-20 09:59:00 +0800

    delete unused files

--- src/control.js
+++ src/control.js
@@ -1,7 +1,7 @@
-import * as d3 from "d3";
-import { main } from "./main";
-import { pmain } from "./index_";
-import { SingleButton } from "./singlebutton";
+import * as d3 from 'd3';
+import { main } from './main';
+import { pmain } from './index_';
+import { SingleButton } from './singlebutton';
 
 main();
 pmain();
@@ -9,21 +9,20 @@ pmain();
 let player = d3.select('#playerframe');
 let team = d3.select('#team');
 
-team.style('visibility', "hidden");
+team.style('visibility', 'hidden');
 
 d3.select('#mode')
-    .attr('width', 100)
-    .attr('height', 24)
-    .attr("transform", "translate(" + 0 + "," + -31 + ")");
+  .attr('width', 100)
+  .attr('height', 24)
+  .attr('transform', 'translate(' + 0 + ',' + -31 + ')');
 let mode = new SingleButton('#mode', ['player', 'team']);
 mode.draw_rect();
 mode.set_listener(() => {
-    if(mode.choosen){
-        team.style('visibility', "visible");
-        player.style('visibility', "hidden");
-    }
-    else {
-        player.style('visibility', "visible");
-        team.style('visibility', "hidden");
-    }
-})
+  if (mode.choosen) {
+    team.style('visibility', 'visible');
+    player.style('visibility', 'hidden');
+  } else {
+    player.style('visibility', 'visible');
+    team.style('visibility', 'hidden');
+  }
+});
