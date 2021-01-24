904348af334f1bed4b454e7aa5d9fc860b8e15ed
fytalon <50952450+FYTalon@users.noreply.github.com>
2021-01-24 10:50:36 +0800

    Update control.js

--- src/control.js
+++ src/control.js
@@ -1,7 +1,10 @@
 import * as d3 from 'd3';
-import { main } from './main';
+import { main, q2 } from './main';
 import { pmain } from './index_';
 import { SingleButton } from './singlebutton';
+import { getActualDim } from "./utils";
+
+let [width, height] = getActualDim('body');
 
 main();
 pmain();
@@ -12,9 +15,9 @@ let team = d3.select('#team');
 team.style('visibility', 'hidden');
 
 d3.select('#mode')
-  .attr('width', 100)
-  .attr('height', 24)
-  .attr('transform', 'translate(' + 0 + ',' + -31 + ')');
+  .attr('width', 0.05 * width)
+  .attr('height', 0.03 * height)
+  .attr('transform', 'translate(' + 0 + ',' + 0.005 * height + ')');
 let mode = new SingleButton('#mode', ['player', 'team']);
 mode.draw_rect();
 mode.set_listener(() => {
